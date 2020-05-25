6b5e61b8094ce299b961a61c5d99152fce0fb68d
赫赫 <1404119786@qq.com>
2020-05-25 22:14:48 +0800

    添加问题详情页面

--- src/main/java/com/code/community/mapper/QuestionMapper.java
+++ src/main/java/com/code/community/mapper/QuestionMapper.java
@@ -3,6 +3,7 @@ package com.code.community.mapper;
 import com.code.community.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,15 +18,18 @@ public interface QuestionMapper {
     public void create(Question question);
 
     @Select("select * from question limit #{offset}, #{size}")
-    List<Question> list(@RequestParam("offset") Integer offset,@RequestParam("size") Integer size);
+    List<Question> list(@Param("offset") Integer offset,@Param("size") Integer size);
 
     @Select("select count(1) from question")
     public Integer count();
 
     @Select("select * from question where creator = #{id} limit #{offset}, #{size}")
-    List<Question> listByUserId(@RequestParam("id") Integer id, @RequestParam("offset") Integer offset, @RequestParam("size")Integer size);
+    List<Question> listByUserId(@Param("id") Integer id, @Param("offset") Integer offset, @RequestParam("size")Integer size);
 
     //根据id查询个人的问题
     @Select("select count(1) from question where creator = #{userId}")
-    Integer countByUserId(@RequestParam("userId")Integer userId);
+    Integer countByUserId(@Param("userId")Integer userId);
+
+    @Select("select * from question where id = #{id}")
+    Question getById(@Param("id")Integer id);
 }
--- src/main/java/com/code/community/service/QuestionService.java
+++ src/main/java/com/code/community/service/QuestionService.java
@@ -85,4 +85,13 @@ public class QuestionService {
         return paginationDTO;
 
     }
+
+    public QuestionDTO findById(Integer id) {
+        Question question = questionMapper.getById(id);
+        QuestionDTO questionDTO = new QuestionDTO();
+        BeanUtils.copyProperties(question, questionDTO);
+        User user = userMapper.findById(question.getCreator());
+        questionDTO.setUser(user);
+        return questionDTO;
+    }
 }
